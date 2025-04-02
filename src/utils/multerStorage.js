ebeea5f604c15e3379900ab117b5f9c84b5436d5
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-04-02 21:10:20 +0300

    Fix upload files

--- src/utils/multerStorage.js
+++ src/utils/multerStorage.js
@@ -1,27 +1,28 @@
+import os from "os";
 import multer from "multer";
 
 const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
+    destination: (req, file, cb) => {
+        cb(null, os.tmpdir());
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
 });
 
 const fileFilter = (req, file, cb) => {
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(
-      new Error(
-        "Invalid file type. Only jpg, jpeg, and png files are allowed."
-      ),
-      false
-    );
-  }
+    if (allowedTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(
+            new Error(
+                "Invalid file type. Only jpg, jpeg, and png files are allowed."
+            ),
+            false
+        );
+    }
 };
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
