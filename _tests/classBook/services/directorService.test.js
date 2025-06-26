bcbab7f27e4b32d08ed5821461935512d9b1ca10
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-06-26 20:06:16 +0300

    Add test for ClassBook directorService

---
+++ _tests/classBook/services/directorService.test.js
@@ -0,0 +1,28 @@
+import { directorService } from "../../../src/services/classBook/directorService.js";
+
+import Setting from "../../../src/models/classBook/Setting.js";
+
+jest.mock("../../../src/models/classBook/Setting.js");
+
+describe("directorService/create()", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("should create a new secretKeys", async () => {
+        const inputData = {
+            teacherKey: "test_teacher_key",
+            directorKey: "test_director_key",
+        };
+        const expectedData = { ...inputData };
+
+        const createdSettings = { _id: "test123", ...expectedData };
+
+        Setting.create.mockResolvedValue(createdSettings);
+
+        const result = await directorService.create(inputData);
+
+        expect(Setting.create).toHaveBeenCalledWith(expectedData);
+        expect(result).toEqual(createdSettings);
+    });
+});
