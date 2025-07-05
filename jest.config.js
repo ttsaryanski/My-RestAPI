c078f686eb5a7541f688dbf9e9ca0598dc0f9e53
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-07-06 00:44:27 +0300

    Babel fix

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,5 @@ export default {
     transformIgnorePatterns: ["/node_modules/(?!mongoose|bson)/"],
     testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
     setupFilesAfterEnv: ["<rootDir>/_tests/test_setup.js"],
+    testEnvironment: "node",
 };
